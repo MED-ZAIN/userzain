676552c6ed3435d237f67117435c7b0f30d11753
MED ZAIN <zain123mohamed2024@gmail.com>
2025-02-02 03:36:48 +0100

    update section again

--- script.js
+++ script.js
@@ -84,5 +84,20 @@ if (savedTheme) {
     themeToggle.textContent = savedTheme === 'dark' ? '☀️' : '🌙';
 }
 
+
+document.getElementById('education-button').addEventListener('click', function() {
+  const content = document.getElementById('education-content');
+  // Toggle the content on mobile
+  if (window.innerWidth <= 768) {
+    this.classList.toggle('clicked');
+    if (this.classList.contains('clicked')) {
+      content.innerHTML = '<h2>Education</h2>';
+    } else {
+      content.innerHTML = '';
+    }
+  }
+});
+
+
 // loading
 
